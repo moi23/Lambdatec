008b3989130d08f19a6ff4915bf71979357f443f
ejunior95 <e.junior95@gmail.com>
2020-03-18 23:04:31 -0300

    [UPDATE] -> Footer adicionado

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/header";
 import Section2 from "./components/section2";
 import Section3 from "./components/section3";
 import Section4 from "./components/section4";
-import H2 from "./components/H2";
 import Footer from "./components/footer";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
-      <H2 />
       <Footer />
     </div> 
   );
--- src/components/footer/styles.js
+++ src/components/footer/styles.js
@@ -13,20 +13,39 @@ export const Container = styled.div `
   }
 
   .LogoLambdaFooter{
-    width: 240px;
-    height: 57px;
+    position: relative;
+    margin-top: 5vh;
+    margin-left: 45%;
+    width: 42px;
+    height: 37px;
   }
 
   .LogoLambdaFooter img {
+    width: 100%;
     height: 100%;
   }
 
   .RedesSociais{
-    background-color: yellow;
+    display: flex;
+    justify-content: center;
+    align-items: center;    
+    margin-top: 10vh;
+    height: 30px;
   }
 
-  .RedesSociais img{
+  .RedesSociais a img{
+    margin-left: 4vh;
+    width: 30px;
+    height: 30px;
+    transition: .3s;
+    transform: scale(1);
+    filter: invert(1);
+    opacity: 30%;
+  }
 
+  .RedesSociais a img:hover {
+    opacity: 100%;
+    transform: scale(1.2);
   }
 
 `;
