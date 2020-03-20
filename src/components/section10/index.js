56dfbe5e2b074ddf0ce6b0269d4fbf52afc5d20a
ejunior95 <e.junior95@gmail.com>
2020-03-20 01:44:41 -0300

    [UPDATE] -> Alterações na sec acima do footer

--- src/components/section10/index.js
+++ src/components/section10/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React from "react";
+import {Container} from "./styles";
 
 export default function section10() {
-  return (
-    <div />
+  return ( 
+  <Container>
+    <h2 className = "ChamadaContrate" > RECEBA UM ORÇAMENTO E CONTRATE A LAMBDA </h2>  
+    <h2 className = "subTituloChamada" > Nossa equipe entrará em contato por e - mail ou telefone para coletar mais informações. </h2>
+
+  </Container>
+
+    
   );
 }
--- src/components/section10/styles.js
+++ src/components/section10/styles.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  .ChamadaContrate{
+    text-align: center;
+    letter-spacing: 15px;
+
+  .subTituloChamada{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+  }
 `;
