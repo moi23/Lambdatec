61b9af8cdb104cc6e5f3412e528222b10a51bea7
ejunior95 <e.junior95@gmail.com>
2020-03-23 23:49:56 -0300

    [MERGED] -> Mescla de versões

--- src/components/section9/index.js
+++ src/components/section9/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { Container } from "./styles";
 
 export default function Section9() {
@@ -22,25 +21,5 @@ export default function Section9() {
         </div>
       </div>
     </Container>
-=======
-import { Container } from "./styles.js";
-
-export default function Section9() {
-  return ( 
-  <Container>
-    <div className="ChamadaFooter">
-      <h2 className ="ChamadaContrate" > RECEBA UM ORÇAMENTO E CONTRATE A LAMBDA </h2>  
-      <h2 className ="subTituloChamada" > Nossa equipe entrará em contato por email ou
-      telefone para coletar mais informações. </h2>
-    </div>
-    <div className="containerBtnContrate">
-      <div className="BtnContrate">
-        {/* <a href="#" target="_blank">CONTRATE NOSSOS SERVIÇOS</a> */}
-      </div>
-    </div>
-  </Container>
-
-    
->>>>>>> edc0da8cd18e5db7ce6a9de7fd0d8ec3e38bfc17
   );
 }
--- src/components/section9/styles.js
+++ src/components/section9/styles.js
@@ -1,17 +1,9 @@
 import styled from 'styled-components';
 
-<<<<<<< HEAD
 export var Container = styled.div`
   background-color: white;
 
   .ChamadaFooter {
-=======
-const Container = styled.div`
- 
-background-color: white;
-
- .ChamadaFooter{
->>>>>>> edc0da8cd18e5db7ce6a9de7fd0d8ec3e38bfc17
     width: 100%;
     padding-top: 10%;
   }
@@ -19,22 +11,13 @@ background-color: white;
   .ChamadaContrate {
     text-align: center;
     letter-spacing: 15px;
-    color: red;
   }
 
-<<<<<<< HEAD
-  .subTituloChamada {
-    text-align: center;
-  }
-
-  .containerBtnContrate {
-=======
   .subTituloChamada{
     text-align: center; 
   }
 
   .containerBtnContrate{
->>>>>>> edc0da8cd18e5db7ce6a9de7fd0d8ec3e38bfc17
     width: 100%;
     display: flex;
     justify-content: center;
@@ -51,31 +34,6 @@ background-color: white;
     line-height: 30px;
     letter-spacing: 8px;
     font-weight: 600;
-<<<<<<< HEAD
-    opacity: 0.9;
-    text-decoration: none;
-    transition: all 0.35s ease-in-out;
-  }
-
-  .BtnContrate a::before {
-    width: 30px;
-    border: 2px solid black;
-    letter-spacing: -0.5px;
-    position: absolute;
-    left: -35px;
-    top: 14px;
-    transition: all 0.35s ease-in-out;
-  }
-
-  .BtnContrate a::after {
-    width: 30px;
-    border: 2px solid black;
-    letter-spacing: -0.5px;
-    position: absolute;
-    right: -28px;
-    top: 14px;
-    transition: all 0.35s ease-in-out;
-=======
     opacity: .9;
     text-decoration: none;
     transition: all .35s ease-in-out;
@@ -101,20 +59,13 @@ background-color: white;
     right: -28px;
     top: 14px;
     transition: all .35s ease-in-out;
->>>>>>> edc0da8cd18e5db7ce6a9de7fd0d8ec3e38bfc17
   }
 
   .BtnContrate a:hover {
     padding-left: 25px;
     padding-right: 25px;
-<<<<<<< HEAD
-    transition: all 0.35s ease-in-out;
-  }
-`;
-=======
     transition: all .35s ease-in-out;
   }
 
 `
->>>>>>> edc0da8cd18e5db7ce6a9de7fd0d8ec3e38bfc17
 export default Container;
